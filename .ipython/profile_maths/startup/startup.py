554d4b7f421d9707ba7f237c991d4de4d418367f
Izaak van Dongen <izaak.van.dongen@gmail.com>
2018-07-24 16:53:00 +0100

    Remove pointless check in maths profile

--- .ipython/profile_maths/startup/startup.py
+++ .ipython/profile_maths/startup/startup.py
@@ -11,11 +11,4 @@ with open(__file__, "r") as setupfile:
 
 print("running on {}".format(sys.executable))
 
-print("reminder from your friendly neighbourhood spiderman to use sage instead"
-      "\ntype the password if you're really sure")
-
-if input() != "the password":
-    print("failure")
-    # apparently sys.exit isn't strong enough
-    import os
-    os.kill(os.getpid(), 9)
+print("reminder from your friendly neighbourhood spiderman to use sage instead")
