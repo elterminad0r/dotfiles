bc81a6c862874ac78ae3dc321ddcc06f4f892568
Izaak van Dongen <izaak.van.dongen@gmail.com>
2018-10-01 13:58:54 +0100

    Add extra imports to maths profile

--- .ipython/profile_maths/startup/startup.py
+++ .ipython/profile_maths/startup/startup.py
@@ -1,3 +1,15 @@
+import sys
+import re
+import random
+import time
+
+from string import *
+from math import *
+from itertools import *
+from functools import *
+from collections import *
+from operator import *
+
 import mpmath
 import scipy
 import numpy as np
