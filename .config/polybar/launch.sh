5a8a58d01884cb45a591dc4ab682fc127721ef76
Izaak van Dongen <izaak.van.dongen@gmail.com>
2019-05-17 18:48:59 +0100

    Add config files for small programs

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# launcher script for polybar, that tears down any already running polybars
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+polybar example #&
+
+echo "Polybar launched..."
