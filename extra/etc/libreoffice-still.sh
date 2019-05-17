87c9e63127e217b317c2d5b11e9b237941c76498
Izaak van Dongen <izaak.van.dongen@gmail.com>
2019-05-17 19:52:23 +0100

    Add etc files

---
+++ extra/etc/libreoffice-still.sh
@@ -0,0 +1,7 @@
+# to force a certain look'n feel
+
+#export SAL_USE_VCLPLUGIN=gen
+#export SAL_USE_VCLPLUGIN=kde4
+#export SAL_USE_VCLPLUGIN=gtk
+export VCL_ICONS_FOR_DARK_THEME=1
+#export SAL_USE_VCLPLUGIN=gtk3
