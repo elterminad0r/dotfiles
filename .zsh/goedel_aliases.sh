786bfcc2f99d4580717ce524bca4bda328e0747d
Izaak van Dongen <izaak.van.dongen@gmail.com>
2019-08-17 16:51:54 +0100

    Add fold alias and get rid of Python suffix alias

--- .zsh/goedel_aliases.sh
+++ .zsh/goedel_aliases.sh
@@ -380,6 +380,9 @@ alias pacman='pacman --color=auto'
 alias p='pacman'
 alias pacsystree='for i in $(pacman -Qeq); do pactree $i; done'
 
+# could also use 'fold -sw 80', although that could break URLs apparently
+alias wrap='fmt -w 80'
+
 # Frivolous aliases
 
 alias rot13='tr "A-Za-z" "N-ZA-Mn-za-m"'
