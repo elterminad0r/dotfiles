f7e0c745e8b8c1d9d3e32f702d1958bc3bf06dee
Izaak van Dongen <izaak.van.dongen@gmail.com>
2019-08-23 15:50:51 +0100

    Add shakespeare capabilities to lipsum

--- pybin/lipsum.py
+++ pybin/lipsum.py
@@ -21,27 +21,40 @@ import pathlib
 
 import smartparse as argparse
 
-from itertools import islice, cycle
+from itertools import islice, cycle, repeat, chain
+
+LIPSUM_DIR = pathlib.Path.home() / "fun" / "lists" / "lipsum"
 
 def get_args():
     """
     Parse argv
     """
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("-f", "--file", type=argparse.FileType("r"),
-            default=str(pathlib.Path.home() /
-                    "fun" / "lists" / "lipsum" / "lipsum.txt"),
+    file_group = parser.add_mutually_exclusive_group()
+    # important that this argument goes first so the default gets triggered
+    file_group.add_argument("-f", "--file", type=argparse.FileType("r"),
+            default=str(LIPSUM_DIR / "lipsum.txt"),
             help="""File to read paragraphs from, one per line, spaced with
                     empty lines""")
+    file_group.add_argument("-s", "--shakespeare", action="store_const",
+            const=(LIPSUM_DIR / "shakespeare.txt").open("r"), dest="file",
+            help="""Print lines from the complete works of Shakespeare""")
     parser.add_argument("paragraphs", type=int, nargs="?", default=5,
-            help="Number of paragraphs to generate")
+            help="""Number of paragraphs to generate. A negative integer `-n` is
+                    taken to mean "n copies of the whole file".""")
     return parser.parse_args()
 
 def lipsum(file, paragraphs):
     """
-    Generate paragraphs, starting back at the beginning if necessary
+    Generate paragraphs, starting back at the beginning if necessary. Follows
+    the semantics detailed in the arguments.
     """
-    for line in islice(cycle(file), paragraphs * 2 - 1):
+    if paragraphs < 0:
+        # have to force the file into a persistent data structure here
+        lines = chain.from_iterable(repeat(list(file), abs(paragraphs)))
+    else:
+        lines = islice(cycle(file), paragraphs * 2 - 1)
+    for line in lines:
         sys.stdout.write(line)
 
 if __name__ == "__main__":
