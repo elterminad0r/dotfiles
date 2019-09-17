c62965ddaff8395dfab24bfb74b485d4a994bf34
Izaak van Dongen <izaak.van.dongen@gmail.com>
2019-09-17 14:47:03 +0100

    High IQ pasta changes

--- pybin/lipsum.py
+++ pybin/lipsum.py
@@ -24,6 +24,23 @@ import smartparse as argparse
 from itertools import islice, cycle, repeat, chain
 
 LIPSUM_DIR = pathlib.Path.home() / "fun" / "lists" / "lipsum"
+PASTA_DIR = pathlib.Path.home() / "fun" / "lists" / "pasta"
+
+PASTA_REGISTRY = {
+        "gamers": PASTA_DIR / "they_targeted_gamers.txt",
+        "gnu/linux": PASTA_DIR / "gnu_linux.txt",
+        "gnu/linux2": PASTA_DIR / "gnu_linux_2.txt"}
+
+def pasta_factory(s):
+    """
+    Wrapper to convert short nicknames to filenames. Can't use argparse choices
+    because it should match the type of the value of the argument.
+    """
+    try:
+        return PASTA_REGISTRY[s].open("r")
+    except KeyError:
+        raise ValueError("Invalid pasta {!r}, choose one of {}".format(
+                s, list(PASTA_REGISTRY)))
 
 def get_args():
     """
@@ -34,15 +51,35 @@ def get_args():
     # important that this argument goes first so the default gets triggered
     file_group.add_argument("-f", "--file", type=argparse.FileType("r"),
             default=str(LIPSUM_DIR / "lipsum.txt"),
-            help="""File to read paragraphs from, one per line, spaced with
-                    empty lines""")
+            help="File to read paragraphs from, spaced with empty lines")
     file_group.add_argument("-s", "--shakespeare", action="store_const",
             const=(LIPSUM_DIR / "shakespeare.txt").open("r"), dest="file",
-            help="""Print lines from the complete works of Shakespeare""")
+            help="Print lines from the complete works of Shakespeare")
+    file_group.add_argument("-p", "--pasta", type=pasta_factory, dest="file",
+            help="Use a copypasta")
     parser.add_argument("paragraphs", type=int, nargs="?", default=5,
             help="""Number of paragraphs to generate. A negative integer `-n` is
                     taken to mean "n copies of the whole file".""")
     return parser.parse_args()
+
+def get_paragraphs(file):
+    """
+    Try to intelligently split up paragraphs from a file
+    """
+    paragraph_body = []
+    paragraph_post = []
+    for line in file:
+        if line.rstrip("\n"):
+            if not paragraph_post:
+                paragraph_body.append(line)
+            else:
+                yield "{}{}".format("".join(paragraph_body),
+                                    "".join(paragraph_post))
+                paragraph_body = [line]
+                paragraph_post = []
+        else:
+            paragraph_post.append(line)
+    yield "{}{}\n".format("".join(paragraph_body), "".join(paragraph_post))
 
 def lipsum(file, paragraphs):
     """
@@ -50,10 +87,11 @@ def lipsum(file, paragraphs):
     the semantics detailed in the arguments.
     """
     if paragraphs < 0:
-        # have to force the file into a persistent data structure here
-        lines = chain.from_iterable(repeat(list(file), abs(paragraphs)))
+        # have to force the file into a persistent data structure here. Add a
+        # newline for good measure
+        lines = chain.from_iterable(repeat([*file, "\n"], abs(paragraphs)))
     else:
-        lines = islice(cycle(file), paragraphs * 2 - 1)
+        lines = islice(cycle(get_paragraphs(file)), paragraphs)
     for line in lines:
         sys.stdout.write(line)
 
