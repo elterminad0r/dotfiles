54343fe9c1d0e4c0db0db110e2a0c3658311f167
Izaak van Dongen <izaak.van.dongen@gmail.com>
2020-10-23 11:36:28 +0100

    Gaga

--- Music/from_yt/nuke_comment_descr.sh
+++ Music/from_yt/nuke_comment_descr.sh
@@ -2,6 +2,14 @@
 
 set -eu
 
-for f in *.m4a; do
+had_args=0
+for f; do
+    had_args=1
     exiftool -comment= -description= "$f"
 done
+
+if [ "$had_args" = 0 ]; then
+    for f in *.m4a; do
+        exiftool -comment= -description= "$f"
+    done
+fi
